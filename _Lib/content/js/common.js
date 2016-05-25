99702d91768b46b4f3adcefd27c141c313534a02
ren8179 <ren8179@126.com>
2016-05-25 10:18:56 +0800

    重命名项目名称

---
+++ _Lib/content/js/common.js
@@ -0,0 +1,14 @@
+﻿seajs.config({
+    base: '/Content/',
+    paths: {
+        'app': 'js/app',
+        'mod': 'js/mod',
+        'lib': 'js/lib'
+    },
+    alias: {
+        'jquery': 'lib/jquery.js?v=20160524',
+        'bootstrap-datepicker': 'lib/datepicker/bootstrap-datepicker.js?v=20160524',
+        'bootstrap-datepicker.zh-CN': 'lib/datepicker/bootstrap-datepicker.zh-CN.js?v=20160524',
+        'bootstrap': 'lib/bootstrap.js?v=20160524'
+    }
+});
---
+++ _Lib/content/js/mod/datepicker.js
@@ -0,0 +1,5 @@
+define(function (require, exports, module) {
+    var $ = require('jquery'),
+        datepicker = require('bootstrap-datepicker'),
+        locale = require('bootstrap-datepicker.zh-CN');
+});
